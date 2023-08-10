b7e1f8a62c63f67dc34868bbccb80685c40fb7be
dihyeonee <xnaxna1@naver.com>
2023-08-10 23:55:53 +0900

    <Study> renderEmpty() 함수 추가

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: getBoxDecoration(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _Logo(),
-            SizedBox(height: 30.0),
-            _AppName(),
-          ],
-        ),
+      body: renderEmpty(),
+    );
+  }
+
+  Widget renderEmpty(){
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(),
+          SizedBox(height: 30.0),
+          _AppName(),
+        ],
       ),
     );
   }
