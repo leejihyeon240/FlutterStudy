6ec7bfb2001da7a31d4855fdc3fd4ba763a6769b
dihyeonee <xnaxna1@naver.com>
2023-08-12 23:56:41 +0900

    <Study> ImagePicker 사용

---
+++ vid_player/lib/component/custom_video_player.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+
+  final XFile video;
+
+  const CustomVideoPlayer({required this.video, Key? key}) : super(key: key);
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoPlayerController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  initializeController() async { // initState()는 async를 사용할 수 없어서 새로운 함수 만들어서 저쪽에서 불러오도록 해야함
+    videoPlayerController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoPlayerController!.initialize();
+
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
