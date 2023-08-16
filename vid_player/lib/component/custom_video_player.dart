f4e664e07510c7d41a8d0b446a377e110dc611dc
dihyeonee <xnaxna1@naver.com>
2023-08-16 23:55:59 +0900

    [Study] 버튼 만들기 및 배치(인데 나는 왜 안 뜨냐 화면에)

--- vid_player/lib/component/custom_video_player.dart
+++ vid_player/lib/component/custom_video_player.dart
@@ -5,7 +5,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
-
   final XFile video;
 
   const CustomVideoPlayer({required this.video, Key? key}) : super(key: key);
@@ -23,29 +22,67 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     super.initState();
   }
 
-  initializeController() async { // initState()는 async를 사용할 수 없어서 새로운 함수 만들어서 저쪽에서 불러오도록 해야함
+  initializeController() async {
+    // initState()는 async를 사용할 수 없어서 새로운 함수 만들어서 저쪽에서 불러오도록 해야함
     videoController = VideoPlayerController.file(
       File(widget.video.path),
     );
 
     await videoController!.initialize();
 
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-
-    if(videoController == null){
+    if (videoController == null) {
       return CircularProgressIndicator(); // 로딩바 띄우기
     }
 
     return AspectRatio(
+      // 원래 비율로 설정
       aspectRatio: videoController!.value.aspectRatio,
-      child: VideoPlayer(
-          videoController!,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoController!,
+          ),
+          _Controls(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Controls extends StatelessWidget {
+  const _Controls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(onPressed: () {}, iconData: Icons.rotate_left),
+          renderIconButton(onPressed: () {}, iconData: Icons.play_arrow),
+          renderIconButton(onPressed: () {}, iconData: Icons.rotate_right),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData, // iconData란, 안에 실제 들어가는 아이콘
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(
+        iconData,
       ),
     );
   }
