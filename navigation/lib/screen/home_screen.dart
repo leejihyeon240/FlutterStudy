120725b3bd0c6ba3c96ebb67c98627b4c5987fb9
dihyeonee <xnaxna1@naver.com>
2023-08-19 23:42:56 +0900

    [Study] 공부할 환경 세팅하기 1

---
+++ navigation/lib/screen/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/screen/route_one_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home Screen'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => RouteOneScreen(),
+                    ),
+                  );
+                },
+                child: Text('push'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
