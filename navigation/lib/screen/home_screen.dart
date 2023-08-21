598e96cd577ff6f08f3080eb196c404dfa385d50
dihyeonee <xnaxna1@naver.com>
2023-08-21 14:12:03 +0900

    [Study] 공부할 환경 세팅하기 2

--- navigation/lib/screen/home_screen.dart
+++ navigation/lib/screen/home_screen.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/screen/route_one_screen.dart';
+import 'package:navigation/layout/main_layout.dart/';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Home Screen'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => RouteOneScreen(),
-                    ),
-                  );
-                },
-                child: Text('push'),
+    return MainLayout(
+      title: 'Home Screen',
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => RouteOneScreen(),
               ),
-            ],
-          ),
-        ));
+            );
+          },
+          child: Text('push'),
+        ),
+      ],
+    );
   }
 }
