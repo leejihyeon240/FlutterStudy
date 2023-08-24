8cae62e098fa012357c5660aa05b31be439e0a39
dihyeonee <xnaxna1@naver.com>
2023-08-24 17:12:00 +0900

    [Study] 알고있으면 좋은 Push 메소드들

--- navigation/lib/screen/route_two_screen.dart
+++ navigation/lib/screen/route_two_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 import '../layout/main_layout.dart';
 
 class RouteTwoScreen extends StatelessWidget {
@@ -22,10 +23,34 @@ class RouteTwoScreen extends StatelessWidget {
           child: Text('Pop'),
         ),
         ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/three', arguments: 999); // named는 바로 arguments로 값 전달 가능
-            },
-            child: Text('Push Named'))
+          onPressed: () {
+            Navigator.of(context).pushNamed('/three',
+                arguments: 999); // named는 바로 arguments로 값 전달 가능
+          },
+          child: Text('Push Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+              // pushReplacement란, RouteTwoScreen를 RouteThreeScreen로 대체
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+            );
+          },
+          child: Text('Push Replacement'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+              (route) => route.settings.name == '/',
+            ); // '/' 제외 삭제
+          },
+          child: Text('Push And Remove Until'),
+        ),
       ],
     );
   }
