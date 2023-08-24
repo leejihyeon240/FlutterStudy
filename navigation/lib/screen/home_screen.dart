9334627208c3539a953617dc3129f1e32088b33e
dihyeonee <xnaxna1@naver.com>
2023-08-24 17:23:17 +0900

    [Study] 알고있으면 좋은 Pop 메소드들

--- navigation/lib/screen/home_screen.dart
+++ navigation/lib/screen/home_screen.dart
@@ -7,22 +7,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => RouteOneScreen(
-                  number: 123,
+    return WillPopScope(
+      onWillPop: () async {
+        // 갤럭시에서 뒤로가기 눌렀을 때 시스템에서 앱 종료 못하도록 방지 하는 것
+        // ture - pop 가능
+        // false - pop 불가능
+        // return false;
+
+        // 작업 실행
+        final canPop = Navigator.of(context).canPop();
+
+        return canPop;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).maybePop(); // 더이상 뒤로 갈 곳이 없을 때 오류 방지
+                // 앱을 끝내버리는 실수를 방지할 수 있음
+              },
+              child: Text('Maybe Pop')),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RouteOneScreen(
+                    number: 123,
+                  ),
                 ),
-              ),
-            );
-          },
-          child: Text('push'),
-        ),
-      ],
+              );
+            },
+            child: Text('push'),
+          ),
+        ],
+      ),
     );
   }
 }
