b6bd6028d90d1391370d8715702da3fd6015c60e
dihyeonee <xnaxna1@naver.com>
2023-08-31 21:03:09 +0900

    [Study] StreamBuilder 위젯 사용하기

--- chool__check/lib/screen/home_screen.dart
+++ chool__check/lib/screen/home_screen.dart
@@ -21,12 +21,12 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 15,
   );
 
-  static final double distance = 100;
+  static final double okDistance = 100;
   static final Circle withinDistanceCircle = Circle(
     circleId: CircleId('withinDistanceCircle'),
     center: companyLatLng,
     fillColor: Colors.blue.withOpacity(0.5),
-    radius: distance,
+    radius: okDistance,
     strokeColor: Colors.blue,
     strokeWidth: 1,
   );
@@ -34,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
     circleId: CircleId('notWithinDistanceCircle'),
     center: companyLatLng,
     fillColor: Colors.red.withOpacity(0.5),
-    radius: distance,
+    radius: okDistance,
     strokeColor: Colors.red,
     strokeWidth: 1,
   );
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
     circleId: CircleId('notWithinDistanceCircle'),
     center: companyLatLng,
     fillColor: Colors.green.withOpacity(0.5),
-    radius: distance,
+    radius: okDistance,
     strokeColor: Colors.green,
     strokeWidth: 1,
   );
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: renderAppBar(),
-        body: FutureBuilder(
+        body: FutureBuilder<String>(
           future: checkPermission(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -65,16 +65,43 @@ class _HomeScreenState extends State<HomeScreen> {
             }
 
             if (snapshot.data == '위치 권한이 허가 되었습니다.') {
-              return Column(
-                children: [
-                  _CustomGoogleMap(
-                    initialPosition: initialPosition,
-                    circle: withinDistanceCircle,
-                    marker: marker,
-                  ),
-                  _ChoolCheckButton(),
-                ],
-              );
+              return StreamBuilder<Position>(
+                  stream: Geolocator.getPositionStream(), // 움직일 때마다 찍힘
+                  builder: (context, snapshot) {
+                    bool isWithinRange =
+                        false; // isWithinRange --> 현재 위치가 원 안에 있는 지?
+
+                    if (snapshot.hasData) {
+                      final start = snapshot.data!; // 현재 위치
+                      final end = companyLatLng;
+
+                      final distance = Geolocator.distanceBetween(
+                        // 거리 계산
+                        start.latitude,
+                        start.longitude,
+                        end.latitude,
+                        end.longitude,
+                      );
+
+                      if (distance < okDistance) {
+                        // 두 거리가 100m(okDistance) 보다 작으면 원 안에 있는 것
+                        isWithinRange = true;
+                      }
+                    }
+
+                    return Column(
+                      children: [
+                        _CustomGoogleMap(
+                          initialPosition: initialPosition,
+                          circle: isWithinRange
+                              ? withinDistanceCircle
+                              : notWithinDistanceCircle,
+                          marker: marker,
+                        ),
+                        _ChoolCheckButton(),
+                      ],
+                    );
+                  });
             }
 
             return Center(
