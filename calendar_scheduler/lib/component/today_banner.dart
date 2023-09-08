92906977bdfb5a121a1db6135bc6ac6e6947c2d3
dihyeonee <xnaxna1@naver.com>
2023-09-08 17:55:47 +0900

    [Study] TodayBanner 설계

--- calendar_scheduler/lib/component/today_banner.dart
+++ calendar_scheduler/lib/component/today_banner.dart
@@ -13,18 +13,28 @@ class TodayBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+    );
+
     return Container(
       color: PRIMARY_COLOR,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            '${selectedDay.year}년 ${selectedDay.month}월 ${selectedDay.day}일',
-          ),
-          Text(
-            '$scheduleCount개',
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${selectedDay.year}년 ${selectedDay.month}월 ${selectedDay.day}일',
+              style: textStyle,
+            ),
+            Text(
+              '$scheduleCount개',
+              style: textStyle,
+            ),
+          ],
+        ),
       ),
     );
   }
