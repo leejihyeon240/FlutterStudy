0335453b6c95e2e8636fbe6a7cd644375f12defc
hidihyeonee <xnaxna1@naver.com>
2024-01-04 23:53:44 +0900

    [Study] 수업준비

--- tabbar_theory/lib/const/tabs.dart
+++ tabbar_theory/lib/const/tabs.dart
@@ -13,4 +13,7 @@ class TabInfo {
 const TABS = [
   TabInfo(icon: Icons.account_balance_outlined, label: '지갑'),
   TabInfo(icon: Icons.alarm_add_outlined, label: '알람'),
+  TabInfo(icon: Icons.keyboard, label: '키보드'),
+  TabInfo(icon: Icons.ac_unit, label: '온도'),
+  TabInfo(icon: Icons.adb, label: '안드로이드'),
 ];
