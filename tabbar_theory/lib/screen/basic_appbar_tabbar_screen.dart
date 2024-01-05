b7147aaa0add845b65f8cf1757574d6ce596b4af
hidihyeonee <xnaxna1@naver.com>
2024-01-05 14:51:02 +0900

    [Study] 기본 탭바

--- tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
+++ tabbar_theory/lib/screen/basic_appbar_tabbar_screen.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../const/tabs.dart';
+
 class BasicAppbarTabbarScreen extends StatelessWidget {
   const BasicAppbarTabbarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+    return DefaultTabController(
+      length: TABS.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BasicAppBarScreen'),
+          bottom: TabBar(
+            tabs: TABS
+                .map((e) => Tab(
+                      icon: Icon(e.icon),
+                      child: Text(e.label),
+                    ))
+                .toList(),
+          ),
+        ),
+        body: Column(
+          children: [],
+        ),
       ),
     );
   }
